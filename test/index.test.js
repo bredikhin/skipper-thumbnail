29a277c5c5a22a186f87daf88cf5e8b29a6c0f73
Ruslan Bredikhin <rus.brn@gmail.com>
2014-07-23 21:52:32 -0400

    more tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,11 +3,21 @@
 require('should');
 
 var Transform = require('stream').Transform;
-var Receiver = require('../')(1, 1);
+var receiver = require('../')(1, 1);
+var Resizer = require('image-resize-stream');
 
 describe('Receiver', function() {
-  it('is a transform stream', function(done) {
-    Receiver.should.be.an.instanceOf(Transform);
+  it('is a transform stream in the object mode', function(done) {
+    receiver.should.be.an.instanceOf(Transform);
+    receiver._writableState.objectMode.should.be.Ok;
+    receiver._readableState.objectMode.should.be.Ok;
+
+    done();
+  });
+
+  it('has a resizing stream attached', function(done) {
+    receiver.should.have.a.property('_resizer');
+    receiver._resizer.should.be.Ok;
 
     done();
   });
